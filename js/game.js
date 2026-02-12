528515d58a23a330829086c9bff63d719042a828
Fabi18742 <geissler.f.a@gmail.com>
2026-02-12 01:07:26 +0100

    Update initialization log message version

--- js/game.js
+++ js/game.js
@@ -46,7 +46,7 @@ const Game = {
 
   // Initialisierung
   init() {
-    console.log("Game wird initialisiert... 0.1");
+    console.log("Game wird initialisiert... 0.1.0");
 
     // Spielstand laden falls vorhanden
     const savedState = Storage.loadGameState();
